b745fe4631212c6e5ce83e8cbbd8d2b67d5dcd5d
Nick Plucker <pluck011@morris.umn.edu>
2017-09-25 18:20:20 -0500

    Edit todo.component.spec.ts

--- client/src/app/todos/todo.component.spec.ts
+++ client/src/app/todos/todo.component.spec.ts
@@ -1,71 +1,71 @@
 import {ComponentFixture, TestBed, async} from "@angular/core/testing";
-import {User} from "./todo";
-import {UserComponent} from "./todo.component";
-import {UserListService} from "./todo-list.service";
+import {todo} from "./todo";
+import {TodoComponent} from "./todo.component";
+import {TodoListService} from "./todo-list.service";
 import {Observable} from "rxjs";
 //import { PipeModule } from "../../pipe.module";
 
-describe("User component", () => {
+describe("Todo component", () => {
 
-    let userComponent: UserComponent;
-    let fixture: ComponentFixture<UserComponent>;
+    let todoComponent: TodoComponent;
+    let fixture: ComponentFixture<TodoComponent>;
 
-    let userListServiceStub: {
-        getUserById: (userId: string) => Observable<User>
+    let todoListServiceStub: {
+        getUserById: (TodoId: string) => Observable<todo>
     };
 
     beforeEach(() => {
         // stub UserService for test purposes
-        userListServiceStub = {
-            getUserById: (userId: string) => Observable.of([
+        todoListServiceStub = {
+            getUserById: (TodoId: string) => Observable.of([
                 {
-                    id: "chris_id",
-                    name: "Chris",
-                    age: 25,
-                    company: "UMM",
-                    email: "chris@this.that"
+                    id: "nick_id",
+                    owner: "Nick",
+                    status: true,
+                    body: "UMM",
+                    category: "student"
                 },
                 {
-                    id: "pat_id",
-                    name: "Pat",
-                    age: 37,
-                    company: "IBM",
-                    email: "pat@something.com"
+                    id: "vipul_id",
+                    owner: "Vipul",
+                    status: false,
+                    body: "UMM",
+                    category: "person"
                 },
                 {
-                    id: "jamie_id",
-                    name: "Jamie",
-                    age: 37,
-                    company: "Frogs, Inc.",
-                    email: "jamie@frogs.com"
+                    id: "nic_id",
+                    owner: "Nic",
+                    status: true,
+                    body: "UMM",
+                    category: "professor"
                 }
-            ].find(user => user.id === userId))
+            ].find(user => user.id === TodoId))
         };
 
         TestBed.configureTestingModule({
             //imports: [PipeModule],
-            declarations: [UserComponent],
-            providers: [{provide: UserListService, useValue: userListServiceStub}]
+            declarations: [TodoComponent],
+            providers: [{provide: TodoListService, useValue: todoListServiceStub}]
         })
     });
 
     beforeEach(async(() => {
         TestBed.compileComponents().then(() => {
-            fixture = TestBed.createComponent(UserComponent);
-            userComponent = fixture.componentInstance;
+            fixture = TestBed.createComponent(TodoComponent);
+            todoComponent = fixture.componentInstance;
         });
     }));
 
-    it("can retrieve Pat by ID", () => {
-        userComponent.setId("pat_id");
-        expect(userComponent.user).toBeDefined();
-        expect(userComponent.user.name).toBe("Pat");
-        expect(userComponent.user.email).toBe("pat@something.com");
+    it("can retrieve Nick by ID", () => {
+        todoComponent.setId("nick_id");
+        expect(todoComponent.todo).toBeDefined();
+        expect(todoComponent.todo.owner).toBe("Nick");
+        expect(todoComponent.user.category).toBe("student");
     });
 
     it("returns undefined for Santa", () => {
-        userComponent.setId("Santa");
-        expect(userComponent.user).not.toBeDefined();
+        todoComponent.setId("Santa");
+        expect(todoComponent.todo).not.toBeDefined();
     });
 
 });
