991bf78fb6c86c83a7a484c6bac05bc89cfe0c94
Nick Plucker <pluck011@morris.umn.edu>
2017-09-26 20:14:36 -0500

    Fix wrong method names with E2E tests

--- client/e2e/todo-list.e2e-spec.ts
+++ client/e2e/todo-list.e2e-spec.ts
@@ -15,59 +15,59 @@ describe('angular-spark-lab', () => {
 
     it('should type something in the Owner box and check that it returned correct element', () => {
         page.navigateTo();
-        page.typeAnOwner("workman");
-        page.toggleSearch();
+        page.typeAOwner("workman");
+        page.selectUpKey();
         expect(page.getFirstTodo()).toEqual("Workman has completed this software design task:");
     });
 
     it('Should select a category and check that it returned correct element', () => {
         page.navigateTo();
-        page.grabACategory('homework');
+        page.typeACategory('homework');
         expect(page.getFirstTodo()).toEqual("Fry has completed this homework task:");
     });
 
     it('Should select a category and check that it returned correct element', () => {
         page.navigateTo();
-        page.grabACategory('software design');
+        page.typeACategory('software design');
         expect(page.getFirstTodo()).toEqual("Blanche has not completed this software design task:");
     });
 
     it('Should select a category and check that it returned correct element', () => {
         page.navigateTo();
-        page.grabACategory('groceries');
+        page.typeACategory('groceries');
         expect(page.getFirstTodo()).toEqual("Blanche has completed this groceries task:");
     });
 
     it('Should select a category and check that it returned correct element', () => {
         page.navigateTo();
-        page.grabACategory('video games');
+        page.typeACategory('video games');
         expect(page.getFirstTodo()).toEqual("Fry has not completed this video games task:");
     });
 
     it('Should select true for status and have the completed todos appear on the page', () => {
         page.navigateTo();
-        page.selectStatus('Complete');
+        page.getTodoByStatus('Complete');
         expect(page.getFirstTodo()).toEqual("Fry has completed this homework task:")
     });
 
     it('Should select false for status and have the incomplete todos appear on the page', () => {
         page.navigateTo();
-        page.selectStatus('Incomplete');
+        page.getTodoByStatus('Incomplete');
         expect(page.getFirstTodo()).toEqual("Blanche has not completed this software design task:")
     });
 
     it('Should only return todos that contain particular content', () => {
         page.navigateTo();
-        page.filterByContent('Excepteur');
+        page.typeABody('Excepteur');
         expect(page.getFirstTodo()).toEqual("Blanche has not completed this homework task:")
     });
 
     it('Should correctly return todos that are filtered by multiple properties', () => {
        page.navigateTo();
-       page.filterByContent('velit');
-       page.selectStatus('Complete');
-       page.typeAnOwner('Workman');
-       page.grabACategory('software design');
+       page.typeABody('velit');
+       page.getTodoByStatus('Complete');
+       page.typeAOwner('Workman');
+       page.typeACategory('software design');
        expect(page.getFirstTodo()).toEqual('Workman has completed this software design task:')
     });
 
